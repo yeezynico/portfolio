b638af1fa553b71b53ae95e383f04b281864b090
yeezynico <yeezy77680@gmail.com>
2024-05-14 21:59:03 +0200

    details

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -12,7 +12,7 @@ const Text = () => {
                 <div className="form-container">
                     <label htmlFor="fname">Sujet:</label>
                     <input type="text"></input>
-                    <textarea id="w3review" name="w3review" rows="4" cols="50">At w3schools.com you will learn how to make a website. They offer free tutorials in all web development technologies.</textarea>
+                    <textarea id="w3review" name="w3review" rows="4" cols="50"></textarea>
                     <br></br>
                     <input type="submit" value="Submit"></input>
                  </div>
